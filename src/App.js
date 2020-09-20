b0a5fa66cf8d824e4c15ce8588488f867c7540e5
Matt Vaccaro <mattvaccaro@Matts-MBP.hsd1.tn.comcast.net>
2020-09-20 14:44:56 -0500

    game rules work

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import './App.css';
 import Grid from './grid';
 
 function App() {
+	const [ gen, setGen ] = useState(0);
 	const [ rows ] = useState(25);
 	const [ cols ] = useState(25);
 	const [ grid, setGrid ] = useState(Array(rows).fill().map(() => Array(cols).fill(false)));
 	const freshGrid = Array(rows).fill().map(() => Array(cols).fill(false));
+	var count = 0;
 
 	function reset() {
 		setGrid(freshGrid);
@@ -31,76 +33,98 @@ function App() {
 				let num = Math.floor(Math.random() * 4);
 				if (num === 1) {
 					gridCopy[R][C] = true;
-					console.log(gridCopy);
 				}
 			}
 		}
 		setGrid(gridCopy);
 	};
 
-	console.log(grid);
+	function counter(arr) {
+		arr.map((cv) => {
+			if (cv === true) {
+				count++;
+			}
+		});
+	}
+
+	function checkNeighbors(R, C, arr) {
+		if (arr[R][C] === false && count == 3) {
+			arr[R][C] = true;
+		}
+		if (arr[R][C] === true && count < 2) {
+			arr[R][C] = false;
+		}
+		if (arr[R][C] === true && count > 3) {
+			arr[R][C] = false;
+		}
+
+		count = 0;
+	}
 
 	function gameMethod() {
-		let g = grid;
 		let gridCopy = arrayClone(grid);
-		var count = 0;
 
 		for (var R = 0; R < rows; R++) {
 			for (var C = 0; C < cols; C++) {
-				// - if [start, start] Do X
+				// - if [R start, C start]
 				if (R === 0 && C === 0) {
-					console.log('Start Start');
+					const section = [ gridCopy[0][1], gridCopy[1][0], gridCopy[1][1] ];
+					counter(section);
+					checkNeighbors(R, C, gridCopy);
 				}
-				// - if [length, start] Do X
-				if (R === 24 && C === 0) {
-					console.log('End Start');
-				}
-				// - if [start, length] Do X
+				// - if [R start, C End]
 				if (R === 0 && C === 24) {
-					console.log('Start End');
+					const section = [ gridCopy[0][23], gridCopy[1][24], gridCopy[1][23] ];
+					counter(section);
+					checkNeighbors(R, C, gridCopy);
 				}
-				// - if [length, length] DO X
+				// - if [R End, C start]
+				if (R === 24 && C === 0) {
+					const section = [ gridCopy[23][0], gridCopy[23][1], gridCopy[24][1] ];
+					counter(section);
+					checkNeighbors(R, C, gridCopy);
+				}
+				// - if [R end, C End]
 				if (R === 24 && C === 24) {
-					console.log('End End');
+					const section = [ gridCopy[24][23], gridCopy[23][23], gridCopy[23][24] ];
+					counter(section);
+					checkNeighbors(R, C, gridCopy);
 				}
 
-				if (R > 0 && R < 24 && C > 0 && C < 24) {
-					const sectionArray = [];
-					sectionArray.push(
-						gridCopy[R - 1][C - 1],
-						gridCopy[R - 1][C],
-						gridCopy[R - 1][C + 1],
-						gridCopy[R][C - 1],
-						gridCopy[R][C + 1],
-						gridCopy[R + 1][C - 1],
-						gridCopy[R + 1][C],
-						gridCopy[R + 1][C + 1]
-					);
-					sectionArray.map((cv) => {
-						if (cv === true) {
-							count = count + 1;
-						}
-					});
+				// Using terns check to see if the current cells neighbors exisit.
+				// Each tern looks for a different section of neighbors.
+				// Undefines get iggnored when counted and are used to ensure react doesn't
+				// throw an error when looking for an index that doesn't exist.
+				const top =
+					typeof gridCopy[R - 1] === 'undefined'
+						? [ undefined ]
+						: [ gridCopy[R - 1][C - 1], gridCopy[R - 1][C], gridCopy[R - 1][C + 1] ];
 
-					if (gridCopy[R][C] === false && count == 3) {
-						gridCopy[R][C] = true;
-					}
-					if (gridCopy[R][C] === true && count < 2) {
-						gridCopy[R][C] = false;
-					}
-					if (gridCopy[R][C] === true && count > 3) {
-						gridCopy[R][C] = false;
-					}
+				const bottom =
+					typeof gridCopy[R + 1] === 'undefined'
+						? `${(R, C)}`
+						: [ gridCopy[R + 1][C - 1], gridCopy[R + 1][C], gridCopy[R + 1][C + 1] ];
 
-					count = 0;
-				}
+				const middle =
+					typeof gridCopy[R][C] === 'undefined' ? `${(R, C)}` : [ gridCopy[R][C - 1], gridCopy[R][C + 1] ];
+
+				const neighbors = top.concat(middle, bottom);
+
+				// Function above that uses a map to count the neighbors
+				counter(neighbors);
+				// Function above that applies the rules of the game and then clears the counter
+				checkNeighbors(R, C, gridCopy);
 			}
 		}
+		// Updates with the copy
 		setGrid(gridCopy);
+		// Updates the Generation count
+		setGen(gen + 1);
 	}
 
 	return (
 		<div className="App">
+			<h1> Generation: {gen} </h1>
 			<Grid grid={grid} rows={rows} cols={cols} selectBox={selectBox} />
 			<button
 				onClick={() => {
